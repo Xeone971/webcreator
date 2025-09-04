ae31ee100e1e9d19abee27d5dd6feb7489a31a5b
Lucca <collas.lucca@outlook.fr>
2025-09-04 21:20:57 +0200

    Configure vite and package.json for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -190,6 +190,7 @@ logger.error = (msg, options) => {
 }
 
 export default defineConfig({
+	base: '/webcreator/',
 	customLogger: logger,
 	plugins: [
 		...(isDev ? [inlineEditPlugin(), editModeDevPlugin()] : []),
